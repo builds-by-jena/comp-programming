d623a800d622f4c6b400b2d73ca612b95eca9618
builds-by-jena <learncodingfast01@gmail.com>
2026-08-02 07:21:55 +0530

    [Codeforces] 71A---Way-Too-Long-Words - Accepted (ID: 385217853)

---
+++ Codeforces/71A---Way-Too-Long-Words/385217853_Attempt_5_AC.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    while (n--) {
+        string word;
+        cin >> word;
+
+        if (word.length() > 10)
+            cout << word.front() << word.length() - 2 << word.back() << "\n";
+        else
+            cout << word << "\n";
+    }
+
+    return 0;
+}//done dusted
