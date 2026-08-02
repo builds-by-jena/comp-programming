b5cf472388a193da54af244fac1c4bf2928c208e
builds-by-jena <learncodingfast01@gmail.com>
2026-08-02 18:16:06 +0530

    [AtCoder] A---Compromise - AC (ID: 78076575)

---
+++ AtCoder/A---Compromise/78076575_Attempt_17_AC.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    bool ok = false;
+
+    for (int i = 0; i < N; i++) {
+        int x;
+        cin >> x;
+        if (x >= 0)
+            ok = true;
+    }
+
+    cout << (ok ? "No" : "Yes") << endl;
+
+    return 0;
+}//123
