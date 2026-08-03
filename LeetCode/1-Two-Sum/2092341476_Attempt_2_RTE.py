afc6cc521a473017962d6d29ccc2f1f915a83323
builds-by-jena <learncodingfast01@gmail.com>
2026-08-03 14:06:28 +0530

    [LeetCode] 1-Two-Sum - Runtime Error (ID: 2092341476)

---
+++ LeetCode/1-Two-Sum/2092341476_Attempt_2_RTE.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums, target):
+        ui = {}
+
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in ui:
+                return [ui[complement], i]
+            ui[num] = iaaa
