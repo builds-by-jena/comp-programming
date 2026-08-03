0e11eea9c90770541ac2591da339169cb932cd37
builds-by-jena <learncodingfast01@gmail.com>
2026-08-03 18:00:59 +0530

    [LeetCode] 6-Zigzag-Conversion - Accepted (ID: 2092591736)

---
+++ LeetCode/6-Zigzag-Conversion/2092591736_Attempt_4_AC.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+
+        if (numRows == 1 || numRows >= s.size())
+            return s;
+
+        vector<string> rows(numRows);
+
+        int cur = 0;
+        bool down = false;
+
+        for (char c : s) {
+
+            rows[cur] += c;
+
+            if (cur == 0 || cur == numRows - 1)
+                down = !down;
+
+            cur += down ? 1 : -1;
+        }
+
+        string ans;
+
+        for (string &row : rows)
+            ans += row;
+
+        return ans;
+    }
+};
