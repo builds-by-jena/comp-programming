54a7b98ab2cf8eb61b6d0d9ad47665a389c2afc7
builds-by-jena <learncodingfast01@gmail.com>
2026-08-04 11:23:50 +0530

    [CodeChef] Print-Squares-Practice-Problem-in-Java - Runtime Error (ID: cc_1785822826397)

---
+++ CodeChef/practice/course/java/LPJAAS02/problems/LJAAS20/cc_1785822826397_Attempt_2_RTE.java
@@ -0,0 +1,10 @@
+class Codechef {
+    public static void main(String[] args) {
+        int x = 10 / 0; // ArithmeticException: / by 
+            zero
+        System.out.println(x);
+    }
+}
+ClassNotFoundException keyword
+CloneNotSupportedException keyword
+NoSuchMethodException keyword
