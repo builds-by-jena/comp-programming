ae542a4d056aad1a8d47cf0a2546a01ef8d2f83e
builds-by-jena <learncodingfast01@gmail.com>
2026-08-04 13:16:20 +0530

    [GeeksforGeeks] Kth-from-End-of-Linked-List - Compilation Error (ID: gfg_1785829574846)

---
+++ GeeksforGeeks/problems/nth-node-from-end-of-linked-list/gfg_1785829574846_Attempt_3_CE.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int getKthFromLast(Node *head, int k) {
+        Node *first = head;
+        Node *second = head;
+
+        // Move first pointer k steps ahead
+        for (int i = 0; i < k; i++) {
+            if (first == NULL)
+                return -1;
+            first = fist->next;
+        }\
+        // Move both pointers until first reaches end
+        while (first != NULL) {
+            first = first->next;
+            second = second->next;
+        }
+
+        return second->data;
+    }
+};
