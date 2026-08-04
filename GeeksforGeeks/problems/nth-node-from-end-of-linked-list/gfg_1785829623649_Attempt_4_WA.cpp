4000ff487705ec5a23731db0ab6ff9ad3bdc6c7e
builds-by-jena <learncodingfast01@gmail.com>
2026-08-04 13:17:09 +0530

    [GeeksforGeeks] Kth-from-End-of-Linked-List - Wrong Answer (ID: gfg_1785829623649)

---
+++ GeeksforGeeks/problems/nth-node-from-end-of-linked-list/gfg_1785829623649_Attempt_4_WA.cpp
@@ -0,0 +1,19 @@
+/*
+class Node {
+   public:
+    int data;
+    Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+} */
+
+class Solution {
+  public:
+    bool detectLoop(Node* head) {
+        // code here
+        
+    }
+};
