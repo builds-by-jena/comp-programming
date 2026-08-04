b5830f31bd53a5d1aa2d124ab179a5087a16a795
builds-by-jena <learncodingfast01@gmail.com>
2026-08-04 11:25:04 +0530

    [CodeChef] Print-Squares-Practice-Problem-in-Java - Compilation Error (ID: cc_1785822876380)

---
+++ CodeChef/practice/course/java/LPJAAS02/problems/LJAAS20/cc_1785822876380_Attempt_3_CE.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public static void main(String[] args) {
+        while (true) {
+            // Infinite loop
+        }
+    }
+}
+ClassNotFoundException keyword
+CloneNotSupportedException keyword
+NoSuchMethodException keyword
